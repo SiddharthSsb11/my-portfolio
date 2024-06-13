a0108f2f066a97a76a9b91a4ea9f97618b0efcf2
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-06-13 23:10:57 +0530

    assets: project assets updated

--- src/assets/images/share/index.js
+++ src/assets/images/share/index.js
@@ -4,6 +4,5 @@ export { default as profile } from "./profile.png";
 export { default as cover } from "./cover.png";
 export { default as logo } from "./logo.png";
 export { default as upload } from "./upload.png";
-export { default as onboarding } from "./onboarding.png";
 export { default as trending } from "./trending.gif";
 export { default as video } from "./video.gif";
