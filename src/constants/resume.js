9b440a6faaf4906b91bb6092914edcd0bf758376
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-09-14 23:58:36 +0530

    docs: resume url updated

--- src/constants/resume.js
+++ src/constants/resume.js
@@ -1,3 +1,3 @@
 const resumeUrl =
-  "https://drive.google.com/file/d/1uCemzbQbCucZL87e4eHbu-tkku8Db8xr/view?usp=sharing";
+  "https://drive.google.com/file/d/1FJuOLPwuQ0JspX7GtUzodiIYr10VSK0G/view?usp=sharing";
 export default resumeUrl;
